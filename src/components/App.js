68cc9708a4b272c0f394f1de35c9f32b0b8043bc
Khum Regmi <88355518+khumRegmi@users.noreply.github.com>
2021-10-16 20:37:26 -0400

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
+
+import PrivateRoute from "./PrivateRoute";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex allign-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "500px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute
+                  path="/update-profile"
+                  component={UpdateProfile}
+                />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+    </AuthProvider>
+  );
+}
+
+export default App;
